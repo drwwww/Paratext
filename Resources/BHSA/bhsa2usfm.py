03b670fda1ec29203ec091f40b2e81b93b7bc492
Drew <31413853+drwww@users.noreply.github.com>
2020-07-08 10:11:21 +0100

    Let there be BHSA
    https://etcbc.github.io/bhsa/

--- Resources/BHSA/bhsa2usfm.py
+++ Resources/BHSA/bhsa2usfm.py
@@ -4,24 +4,36 @@ import codecs, os
 
 path = "C:\\Users\\philo\\OneDrive\\Web\\Github\\Paratext\\Resources\\BHSA\\"
 
-book_num_dict = {'01': 'GEN', '02': 'EXO', '03': 'LEV', '04': 'NUM', '05': 'DEU', '06': 'JOS', '07': 'JDG', '08': 'RUT', '09': '1SA', '10': '2SA', '11': '1KI', '12': '2KI', '13': '1CH', '14': '2CH', '15': 'EZR', '16': 'NEH', '17': 'EST', '18': 'JOB', '19': 'PSA', '20': 'PRO', '21': 'ECC', '22': 'SNG', '23': 'ISA', '24': 'JER', '25': 'LAM', '26': 'EZK', '27': 'DAN', '28': 'HOS', '29': 'JOL', '30': 'AMO', '31': 'OBA', '32': 'JON', '33': 'MIC', '34': 'NAM', '35': 'HAB', '36': 'ZEP', '37': 'HAG', '38': 'ZEC', '39': 'MAL'}
+bhsa_book_list_with_ptx_ids = ['GEN', 'EXO', 'LEV', 'NUM', 'DEU', 'JOS', 'JDG', '1SA', '2SA', '1KI', '2KI', 'ISA', 'JER', 'EZK', 'HOS', 'JOL', 'AMO', 'OBA', 'JON', 'MIC', 'NAM', 'HAB', 'ZEP', 'HAG', 'ZEC', 'MAL', 'PSA', 'JOB', 'PRO', 'RUT', 'SNG', 'ECC', 'LAM', 'EST', 'DAN', 'EZR', 'NEH', '1CH', '2CH']
+book_num_dict = {'GEN': '01', 'EXO': '02', 'LEV': '03', 'NUM': '04', 'DEU': '05', 'JOS': '06', 'JDG': '07', 'RUT': '08', '1SA': '09', '2SA': '10', '1KI': '11', '2KI': '12', '1CH': '13', '2CH': '14', 'EZR': '15', 'NEH': '16', 'EST': '17', 'JOB': '18', 'PSA': '19', 'PRO': '20', 'ECC': '21', 'SNG': '22', 'ISA': '23', 'JER': '24', 'LAM': '25', 'EZK': '26', 'DAN': '27', 'HOS': '28', 'JOL': '29', 'AMO': '30', 'OBA': '31', 'JON': '32', 'MIC': '33', 'NAM': '34', 'HAB': '35', 'ZEP': '36', 'HAG': '37', 'ZEC': '38', 'MAL': '39'}
+ptx_ids_bhs_names = {'GEN': 'בראשית', 'EXO': 'שמות', 'LEV': 'ויקרא', 'NUM': 'במדבר', 'DEU': 'דברים', 'JOS': 'יהושע', 'JDG': 'שפטים', '1SA': 'שמואל א', '2SA': 'שמואל ב', '1KI': 'מלכים א', '2KI': 'מלכים ב', 'ISA': 'ישעיהו', 'JER': 'ירמיהו', 'EZK': 'יחזקאל', 'HOS': 'הושע', 'JOL': 'יואל', 'AMO': 'עמוס', 'OBA': 'עבדיה', 'JON': 'יונה', 'MIC': 'מיכה', 'NAM': 'נחום', 'HAB': 'חבקוק', 'ZEP': 'צפניה', 'HAG': 'חגי', 'ZEC': 'זכריה', 'MAL': 'מלאכי', 'PSA': 'תהלים', 'JOB': 'איוב', 'PRO': 'משלי', 'RUT': 'רות', 'SNG': 'שיר השירים', 'ECC': 'קהלת', 'LAM': 'איכה', 'EST': 'אסתר', 'DAN': 'דניאל', 'EZR': 'עזרא', 'NEH': 'נחמיה', '1CH': 'דברי הימים א', '2CH': 'דברי הימים ב'}
+ptx_ids_latin_names = {'GEN': 'Genesis', 'EXO': 'Exodus', 'LEV': 'Leviticus', 'NUM': 'Numeri', 'DEU': 'Deuteronomium', 'JOS': 'Josua', 'JDG': 'Judices', '1SA': 'Samuel I', '2SA': 'Samuel II', '1KI': 'Reges I', '2KI': 'Reges II', 'ISA': 'Jesaia', 'JER': 'Jeremia', 'EZK': 'Ezechiel', 'HOS': 'Hosea', 'JOL': 'Joel', 'AMO': 'Amos', 'OBA': 'Obadia', 'JON': 'Jona', 'MIC': 'Micha', 'NAM': 'Nahum', 'HAB': 'Habakuk', 'ZEP': 'Zephania', 'HAG': 'Haggai', 'ZEC': 'Sacharia', 'MAL': 'Maleachi', 'PSA': 'Psalmi', 'JOB': 'Iob', 'PRO': 'Proverbia', 'RUT': 'Ruth', 'SNG': 'Canticum', 'ECC': 'Ecclesiastes', 'LAM': 'Threni', 'EST': 'Esther', 'DAN': 'Daniel', 'EZR': 'Esra', 'NEH': 'Nehemia', '1CH': 'Chronica I', '2CH': 'Chronica II'}
+#num_book_dict = {'01': 'GEN', '02': 'EXO', '03': 'LEV', '04': 'NUM', '05': 'DEU', '06': 'JOS', '07': 'JDG', '08': 'RUT', '09': '1SA', '10': '2SA', '11': '1KI', '12': '2KI', '13': '1CH', '14': '2CH', '15': 'EZR', '16': 'NEH', '17': 'EST', '18': 'JOB', '19': 'PSA', '20': 'PRO', '21': 'ECC', '22': 'SNG', '23': 'ISA', '24': 'JER', '25': 'LAM', '26': 'EZK', '27': 'DAN', '28': 'HOS', '29': 'JOL', '30': 'AMO', '31': 'OBA', '32': 'JON', '33': 'MIC', '34': 'NAM', '35': 'HAB', '36': 'ZEP', '37': 'HAG', '38': 'ZEC', '39': 'MAL'}
 
 A = use('bhsa', hoist=globals())
 
 for i, b in enumerate(F.otype.s('book')):
     book = T.bookName(b)
-    if "Ruth" in book:
-        book_num = str(f"{i+1:02d}")
-        # Write file
-        filename = "SFMs\\" + str(i+1) + book_num_dict[book_num] + "BHSA" + ".sfm"
-        full_filename = path + filename
-        with codecs.open(full_filename, "w", "utf-8") as file:
-            file.write(f"\\id {book_num_dict[book_num]} - BHSA\n\\h {book}\n\\toc2 {book_num_dict[book_num]}\n\\toc1 {book}\n\\mt {book}\n")
+    if "_" in book:
+        book = book.replace("_", " ").replace("1", "I").replace("2", "II")              
+    book_ptx_abrev = bhsa_book_list_with_ptx_ids[i]
+    book_num = book_num_dict[book_ptx_abrev]
+    book_heb = ptx_ids_bhs_names[book_ptx_abrev]
+    book_latin = ptx_ids_latin_names[book_ptx_abrev]
+    #if "Chron" in book:
+    # print(book)
+    # Write file
+    filename = "SFMs\\" + book_num + book_ptx_abrev + "BHSA" + ".sfm"
+    full_filename = path + filename
+    with codecs.open(full_filename, "w", "utf-8") as file:
+        file.write(f"\\id {book_ptx_abrev} - Biblia Hebraica Stuttgartensia (Amstelodamensis)\n\\h {book.title()}\n\\toc3 {book_ptx_abrev}\n\\toc2 {book.title()}\n\\toc1 {book_heb}\n\\mt1 {book_heb}\n\\mt2 {book_latin}\n\\mt3 {book.title()}\n")
         #print(book)
-            for i, chap in enumerate(L.d(b, 'chapter')):
-                #print(f"\\c {i+1}\n\\p")
-                file.write(f"\\c {i+1}\n\\p ")
-                for i, verse in enumerate(L.d(chap, 'verse')):
-                    #print(f"\\v {i+1}")
-                    verse_text = get_display(T.text(verse))
-                    file.write(f"\\v {i+1} {verse_text}\n")
+        for i, chap in enumerate(L.d(b, 'chapter')):
+            #print(f"\\c {i+1}\n\\p")
+            file.write(f"\\c {i+1}\n")
+            for i, verse in enumerate(L.d(chap, 'verse')):
+                #print(f"\\v {i+1}")
+                #verse_text = get_display(T.text(verse))
+                #file.write(f"\\v {i+1} {verse_text}\n")
+                #file.write(f"{verse_text} {i+1} v\\\n")
+                file.write(f"\\q \\v {i+1} {T.text(verse)}\n")
--- USFM Book Identifiers/OT book abbreviations and numbers for filenames.py
+++ USFM Book Identifiers/OT book abbreviations and numbers for filenames.py
@@ -1,45 +1,69 @@
+# Paratext book abbreviations in Western/Protestant order
 books = "GEN,EXO,LEV,NUM,DEU,JOS,JDG,RUT,1SA,2SA,1KI,2KI,1CH,2CH,EZR,NEH,EST,JOB,PSA,PRO,ECC,SNG,ISA,JER,LAM,EZK,DAN,HOS,JOL,AMO,OBA,JON,MIC,NAM,HAB,ZEP,HAG,ZEC,MAL"
 book_list = books.split(",")
 
-nums = ("01",
-"02",
-"03",
-"04",
-"05",
-"06",
-"07",
-"08",
-"09",
-"10",
-"11",
-"12",
-"13",
-"14",
-"15",
-"16",
-"17",
-"18",
-"19",
-"20",
-"21",
-"22",
-"23",
-"24",
-"25",
-"26",
-"27",
-"28",
-"29",
-"30",
-"31",
-"32",
-"33",
-"34",
-"35",
-"36",
-"37",
-"38",
-"39",)
+# Paratext book abbreviations in BHSA book order
+bhsa_book_order = "GEN,EXO,LEV,NUM,DEU,JOS,JDG,1SA,2SA,1KI,2KI,ISA,JER,EZK,HOS,JOL,AMO,OBA,JON,MIC,NAM,HAB,ZEP,HAG,ZEC,MAL,PSA,JOB,PRO,RUT,SNG,ECC,LAM,EST,DAN,EZR,NEH,1CH,2CH"
+bhsa_book_list = bhsa_book_order.split(",")
+#print(bhsa_book_list)
 
-dicty = dict(zip(nums, book_list))
+# Latin book names in BHS order
+latin_book_names = "Genesis,Exodus,Leviticus,Numeri,Deuteronomium,Josua,Judices,Samuel I,Samuel II,Reges I,Reges II,Jesaia,Jeremia,Ezechiel,Hosea,Joel,Amos,Obadia,Jona,Micha,Nahum,Habakuk,Zephania,Haggai,Sacharia,Maleachi,Psalmi,Iob,Proverbia,Ruth,Canticum,Ecclesiastes,Threni,Esther,Daniel,Esra,Nehemia,Chronica I,Chronica II"
+latin_book_list = latin_book_names.split(",")
+
+# Hebrew book names in BHS order
+bhs_book_names = [
+"בראשית",
+"שמות",
+"ויקרא",
+"במדבר",
+"דברים",
+"יהושע",
+"שפטים",
+"שמואל א",
+"שמואל ב",
+"מלכים א",
+"מלכים ב",
+"ישעיהו",
+"ירמיהו",
+"יחזקאל",
+"הושע",
+"יואל",
+"עמוס",
+"עבדיה",
+"יונה",
+"מיכה",
+"נחום",
+"חבקוק",
+"צפניה",
+"חגי",
+"זכריה",
+"מלאכי",
+"תהלים",
+"איוב",
+"משלי",
+"רות",
+"שיר השירים",
+"קהלת",
+"איכה",
+"אסתר",
+"דניאל",
+"עזרא",
+"נחמיה",
+"דברי הימים א",
+"דברי הימים ב"]
+
+# Paratext book numbers in Western/Protestant order
+nums = ("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39")
+
+#dicty = dict(zip(nums, book_list))
+# dicty = dict(zip(book_list, nums))
+# print(dicty)
+
+# Print bhsa_book_list with bhs_book_names as dict
+# dicty = dict(zip(bhsa_book_list, bhs_book_names))
+# print(dicty)
+
+# Print bhsa_book_list with latin_book_list with as dict
+dicty = dict(zip(bhsa_book_list, latin_book_list))
 print(dicty)
